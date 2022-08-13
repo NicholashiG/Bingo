d576a53ef4d797ad838627be03f410a633560fd4
Nicolás Jurado <nicomacojurado@gmail.com>
2022-08-13 11:38:25 -0500

    Update VentanaBingo.java

--- co/uniquindio/programacion/bingo/view/VentanaBingo.java
+++ co/uniquindio/programacion/bingo/view/VentanaBingo.java
@@ -1,18 +1,12 @@
 package co.uniquindio.programacion.bingo.view;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-
 import co.uniquindio.programacion.bingo.model.Bingo;
 
 /**
